5e6aecdefa6723bf2bc2db6fa2f8aada375a065a
Rafael Walter <rafawalter@gmail.com>
2018-02-28 23:04:55 -0300

    D2: Connecting Products to Carts

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,17 @@ class Product < ApplicationRecord
     with: /\.(gif|jpg|png)\Z/i,
     message: 'deve ser a URL de uma imagem GIF, JPG ou PNG.'
   }
+
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end
 end
