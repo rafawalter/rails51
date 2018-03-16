39cb5f861ee9bd9daba7fe7cd2deee5832211f61
Rafael Walter <rafawalter@gmail.com>
2018-03-16 07:37:48 -0300

    H2. Testing our javascript funcionality

--- test/system/carts_test.rb
+++ test/system/carts_test.rb
@@ -1,9 +1,18 @@
 require "application_system_test_case"
 
 class CartsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit carts_url
-  #
-  #   assert_selector "h1", text: "Cart"
-  # end
+  test 'add to cart and empty cart should reveal and hide the cart' do
+    CART_LOCATOR = '#cart article'
+
+    visit store_index_url
+    assert_no_selector CART_LOCATOR
+
+    first('.catalog li').click_on 'Add to Cart'
+    assert_selector CART_LOCATOR
+
+    accept_alert do
+      click_on 'Empty cart'
+    end
+    assert_no_selector CART_LOCATOR
+  end
 end
