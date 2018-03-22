5a3d19125500d369a990254d171827ffdc2b22e4
Rafael Walter <rafawalter@gmail.com>
2018-03-22 08:13:17 -0300

    J2: Authenticating Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
   resources :users
   resources :orders
   resources :line_items
