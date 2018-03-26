ec00ddf11ac7666361b127535bff1603df5ee8cb
Rafael Walter <rafawalter@gmail.com>
2018-03-26 07:29:23 -0300

    K1: Selecting the Locale

--- config/routes.rb
+++ config/routes.rb
@@ -9,16 +9,16 @@ Rails.application.routes.draw do
   end
 
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
-
-  get 'store/index'
 
   resources :products do
     get :who_bought, on: :member
   end
 
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
 
 end
